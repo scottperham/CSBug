ef5f52e001f45173c5c7cc86b7d46f3b09063e4b
Scott Perham <scott.perham@gmail.com>
2016-09-08 23:52:32 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ret = true ? null : new ClassWithImplicitOperator();
+            Console.WriteLine(ret);
+            Console.ReadLine();
+        }
+    }
+
+    public class ClassWithImplicitOperator
+    {
+        public static implicit operator string (ClassWithImplicitOperator other)
+        {
+            Console.WriteLine("Implicitly casting...");
+            return "";
+        }
+    }
+}
